d195fe126ac45f601c95ef9f9cb05b8b0b1bbc1a
colletha <20110010@colegioscj.edu.gt>
2025-08-19 10:23:01 -0600

    Usuario Model

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -1,17 +1,10 @@
-<?php
+<?php 
 namespace App\Models;
+
 use CodeIgniter\Model;
-class UsuarioModel extends Model
-{
+
+class UsuarioModel extends Model {
     protected $table = 'usuarios';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['usuario', 'password'];
-    public function verificarUsuario($usuario, $password)
-    {
-        return $this->where('usuario', $usuario)
-        ->where('password', md5($password))
-
-->first();
-
-}
+    protected $allowedFields = ['nombre', 'email', 'password', 'rol'];
 }
