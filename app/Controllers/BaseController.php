3a576616b78eca5470106d2cc9ab91b6d5c6de36
colletha <20110010@colegioscj.edu.gt>
2025-08-19 10:10:38 -0600

    primera modificación

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,58 +1,40 @@
 <?php
-
 namespace App\Controllers;
-
-use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use Psr\Log\LoggerInterface;
-
-/**
- * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
- */
-abstract class BaseController extends Controller
+use App\Models\UsuarioModel;
+class Login extends BaseController
 {
-    /**
-     * Instance of the main Request object.
-     *
-     * @var CLIRequest|IncomingRequest
-     */
-    protected $request;
-
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
-     * @var list<string>
-     */
-    protected $helpers = [];
-
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
-
-    /**
-     * @return void
-     */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-    {
-        // Do Not Edit This Line
-        parent::initController($request, $response, $logger);
-
-        // Preload any models, libraries, etc, here.
-
-        // E.g.: $this->session = service('session');
-    }
+public function index()
+{
+return view('login');
+}
+public function autenticar()
+{
+$usuario = $this->request->getPost('usuario');
+$password = $this->request->getPost('password');
+$usuarioModel = new UsuarioModel();
+$datosUsuario = $usuarioModel->verificarUsuario($usuario,
+$password);
+if ($datosUsuario) {
+session()->set([
+'usuario' => $datosUsuario['usuario'],
+'logged_in' => true
+]);
+return redirect()->to('/panel');
+} else {
+return redirect()->back()->with('error', 'Usuario o
+contraseña incorrectos');
+}
+}
+public function panel()
+{
+if (!session()->get('logged_in')) {
+return redirect()->to('/');
+}
+return view('panel');
+}
+public function salir()
+{
+session()->destroy();
+return redirect()->to('/');
+}
 }
